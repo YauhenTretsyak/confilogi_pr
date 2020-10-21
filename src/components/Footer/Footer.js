55c16b7aff02bd4d58401770a6962c7b356b5919
Yauhen <zhendos@tut.by>
2020-10-21 16:49:00 +0200

    rewrite structure of sass files

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+const Footer = () => {
+  return (
+    <section className="footer">
+      <div className="box_wrapper">
+
+      </div>
+    </section>
+  )
+};
+
+export default Footer;
