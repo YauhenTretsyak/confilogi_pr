4251d03574777bd071d1405aea5cc933562af9ca
Yauhen <zhendos@tut.by>
2020-10-22 16:59:17 +0200

    modified blogSearchItem component

--- src/components/BlogPosts/BlogPosts.js
+++ src/components/BlogPosts/BlogPosts.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import BlogHeader from '../blocks/BlogHeader/BlogHeader.js';
 import BlogPostItem from '../blocks/BlogPostItem/BlogPostItem.js';
+import BlogSearchItem from '../blocks/BlogSearchItem/BlogSearchItem.js';
+
 import imgTestLink from '../../assets/photo/aside-dzieki-tym.png';
 import imgTestLink2 from '../../assets/photo/aside-szokujace-wyznanie.png';
 import imgTestLink3 from '../../assets/photo/aside-poznaj-historie.png';
@@ -11,7 +13,7 @@ const BlogPosts = () => {
   const testContent = 'Dzięki tym prostym ćwiczeniom ujędrnisz pośladki zaledwie w 2 tygodnie!';
   const testContent2 = 'Szokujące wyznanie modelki plus size – byłam anorektyczką i nie miałam biustu do 25 roku życia.';
   const testContent3 = 'Poznaj historię czytelniczki, która szybko wróciła do formy';
-
+  const testContentSearch = 'Bądź z nami na bieżąco! Zapisz się do newslettera. Codziennie nowe fakty z życia gwiazd!'
 
   return (
     <section className='blog_posts'>
@@ -21,6 +23,18 @@ const BlogPosts = () => {
       <BlogPostItem imgUrl={ imgTestLink } content={ testContent } />
       <BlogPostItem imgUrl={ imgTestLink2 } content={ testContent2 } />
       <BlogPostItem imgUrl={ imgTestLink3 } content={ testContent3 } />
+
+      <BlogSearchItem 
+        title={'Wyszukaj'}
+        placeHolderTitle={'Wpisz frazę...'}
+        btnTitle={'Szukaj'}
+      />
+      <BlogSearchItem 
+        title={'Newsletter'} 
+        placeHolderTitle={'Wpisz adres e-mail...'}
+        btnTitle={'Zapisz'}
+        content={ testContentSearch }
+      />
 
     </section>
   )
