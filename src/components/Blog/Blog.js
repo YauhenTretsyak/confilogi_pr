2b7d655cd5a19cd2a0579c3aa5512fdf191b072b
Agata Nakonieczna <agnakonieczna@gmail.com>
2020-10-22 13:14:42 +0200

    Blog component changed, Blog styles added, Button component styles added, fonts added to typography

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import Button from '../blocks/Button/Button.js';
+import Navigation from '../blocks/Navigation/Navigation.js';
+import mainPhoto from '../../assets/photo/main-photo.png';
 
 const Blog = () => {
   return (
     <section className='blog'>
-      <p></p>
-      <Button text='Przemiany gwiazd' />
-      {/* <Date /> */}
-      {/* <h1></h1>
-      <img></img> */}
+      <div className='blog_links'>
+        <Navigation text={'Aktualności'} styleName={'blog_links_item'} /> / <Navigation text={'Przemiany gwiazd'} styleName={'blog_links_item'} /> / <span>"Wow! Niesamowity sekret Kylie..."</span>
+      </div>
+      <Button styleName='blog_btn' text='Przemiany gwiazd' />
+      <div className='blog_info'>{/* <Date /> */}</div>
+      <h1 className='blog_title'>
+        Wow! Niesamowity sekret Kylie Jenner odkryty – dzięki <span className='pink-text'>TEJ</span>{' '}
+        prostej metodzie z łatwością powiększysz i ujędrnisz biust oraz pośladki
+        tak jak ona!
+      </h1>
+      <img className='blog_img' src={mainPhoto} alt='kylie-jenner'></img>
+      {/* <Paragraph /> */}
     </section>
-  )
+  );
 };
 
 export default Blog;
--- src/components/blocks/Button/Button.js
+++ src/components/blocks/Button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Button = (props) => {
-  return <button className='btn'>{props.text}</button>;
+const Button = ({text, styleName}) => {
+  return <button className={`btn ${styleName}`}>{text}</button>;
 };
 
 export default Button;
