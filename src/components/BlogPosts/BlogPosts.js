1b2f6e6b6afd1863aa804a193eba33012c465846
Yauhen <zhendos@tut.by>
2020-10-22 15:05:09 +0200

    modified Date

--- src/components/BlogPosts/BlogPosts.js
+++ src/components/BlogPosts/BlogPosts.js
@@ -5,13 +5,19 @@ import imgTestLink from '../../assets/photo/aside-dzieki-tym.png'
 
 
 const BlogPosts = () => {
+
+  const testContent = 'Dzięki tym prostym ćwiczeniom ujędrnisz pośladki zaledwie w 2 tygodnie!'
+
   return (
     <section className='blog_posts'>
       <BlogHeader 
         typeHeader={'blog_header'} 
         title={'Ostatnie posty'}
       />
-      <BlogPostItem imgUrl={ imgTestLink } />
+      <BlogPostItem imgUrl={ imgTestLink } content={ testContent } />
+      <BlogPostItem imgUrl={ imgTestLink } content={ testContent } />
+      <BlogPostItem imgUrl={ imgTestLink } content={ testContent } />
+
     </section>
   )
 };
--- src/components/blocks/BlogPostItem/BlogPostItem.js
+++ src/components/blocks/BlogPostItem/BlogPostItem.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import Date from '../Date/Date.js';
 
 const BlogPostItem = ({imgUrl, content}) => {
   return(
     <>
       <div className='blog_post_item'>
-        <img src={imgUrl} alt='post img'></img>
+        <div className='blog_post_item__img-wrapper'>
+          <img className='blog_post_item__img' src={imgUrl} alt='post img'></img>
+        </div>
         <div className='blog_post_item__content-wrapper'>
-          <p>{ content }</p>
-          {/* <Date /> */}
+          <p className='blog_post_item__content'>{ content }</p>
+          <div className='blog_post_item__date_wrapper'>
+            <Date 
+              classnameDate={'date-blog-posts'}
+              classnameText={'date-blog-posts_text'}
+            />
+          </div>
         </div>
       </div>
     </>
