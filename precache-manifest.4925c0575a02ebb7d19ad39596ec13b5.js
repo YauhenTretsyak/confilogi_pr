8e96ea1f06ec0f92221f1d02dbc3f128dd43efd9
Yauhen <zhendos@tut.by>
2020-10-23 09:31:57 +0200

    Updates

---
+++ precache-manifest.4925c0575a02ebb7d19ad39596ec13b5.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "723a52b23e4f9b3d55775b0f459af82f",
+    "url": "/confilogi_pr/index.html"
+  },
+  {
+    "revision": "d2fb887ed5d5da8871df",
+    "url": "/confilogi_pr/static/css/main.d68bde1b.chunk.css"
+  },
+  {
+    "revision": "7eda449e1d1447281973",
+    "url": "/confilogi_pr/static/js/2.6d6643a5.chunk.js"
+  },
+  {
+    "revision": "48bd7bad75130283b8e683aa665baa5c",
+    "url": "/confilogi_pr/static/js/2.6d6643a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2fb887ed5d5da8871df",
+    "url": "/confilogi_pr/static/js/main.9ba166b7.chunk.js"
+  },
+  {
+    "revision": "c648aba5b51fe355b338",
+    "url": "/confilogi_pr/static/js/runtime-main.1629f126.js"
+  },
+  {
+    "revision": "fabc01972a8a1955e45a2ae496e99415",
+    "url": "/confilogi_pr/static/media/Abuget.fabc0197.woff2"
+  },
+  {
+    "revision": "fdf547429ba0772007afbcaf957405bb",
+    "url": "/confilogi_pr/static/media/Abuget.fdf54742.woff"
+  },
+  {
+    "revision": "22ac76f0bcc89497c9b37517ba27120b",
+    "url": "/confilogi_pr/static/media/AmsiProNarw-BlackItalic.22ac76f0.woff2"
+  },
+  {
+    "revision": "765efc957dad965c85d27fb42ba936e1",
+    "url": "/confilogi_pr/static/media/AmsiProNarw-BlackItalic.765efc95.woff"
+  },
+  {
+    "revision": "242780083d90e494fa8a5f41a6a4d2cd",
+    "url": "/confilogi_pr/static/media/Montserrat-Bold.24278008.woff"
+  },
+  {
+    "revision": "35b1efc06970f9de3ea323ab868133a1",
+    "url": "/confilogi_pr/static/media/Montserrat-Bold.35b1efc0.woff2"
+  },
+  {
+    "revision": "685196d199b1f5f41b71ca3f0f444952",
+    "url": "/confilogi_pr/static/media/SegoeUi-Regular.685196d1.woff2"
+  },
+  {
+    "revision": "fa1aa4adf2dbf5453a82c10bf021aa2a",
+    "url": "/confilogi_pr/static/media/SegoeUi-Regular.fa1aa4ad.woff"
+  },
+  {
+    "revision": "32a178fdb788c5e51e9ac55a9036c4f0",
+    "url": "/confilogi_pr/static/media/aside-dzieki-tym.32a178fd.png"
+  },
+  {
+    "revision": "4a5b609c23f2a79980753d1b8b9bcb6f",
+    "url": "/confilogi_pr/static/media/aside-poznaj-historie.4a5b609c.png"
+  },
+  {
+    "revision": "423b1bc05642ebcadba82fe46f17dfcb",
+    "url": "/confilogi_pr/static/media/aside-szokujace-wyznanie.423b1bc0.png"
+  },
+  {
+    "revision": "0fb6fa34e27e11004971f8ef7c07a722",
+    "url": "/confilogi_pr/static/media/icon-author.0fb6fa34.svg"
+  },
+  {
+    "revision": "61a97845915d5db03fcc22cf0d7b6626",
+    "url": "/confilogi_pr/static/media/icon-clock.61a97845.svg"
+  },
+  {
+    "revision": "ce4ba31988a624c1ec3e982de648620b",
+    "url": "/confilogi_pr/static/media/icon-facebook.ce4ba319.svg"
+  },
+  {
+    "revision": "39b15bf9d39f2de8edc3b1caeaeaf8c1",
+    "url": "/confilogi_pr/static/media/icon-instagram.39b15bf9.svg"
+  },
+  {
+    "revision": "de158395a997557507497c95921d2d51",
+    "url": "/confilogi_pr/static/media/icon-twitter.de158395.svg"
+  },
+  {
+    "revision": "5f562db97b7d0adea556968b0e7a5852",
+    "url": "/confilogi_pr/static/media/main-photo.5f562db9.png"
+  },
+  {
+    "revision": "6311a1d44670a3c2741855fb837dd844",
+    "url": "/confilogi_pr/static/media/myriadpro-bold.6311a1d4.woff"
+  },
+  {
+    "revision": "82c3b5f3be6c5fb9c975667e7c9c4c3a",
+    "url": "/confilogi_pr/static/media/myriadpro-bold.82c3b5f3.woff2"
+  },
+  {
+    "revision": "75499347876c2566776c4fed05f88c9a",
+    "url": "/confilogi_pr/static/media/myriadpro-light.75499347.woff2"
+  },
+  {
+    "revision": "b1307482a5c1aba623fdf1502fbe303e",
+    "url": "/confilogi_pr/static/media/myriadpro-light.b1307482.woff"
+  },
+  {
+    "revision": "bf59179a69401466f66a402878af862e",
+    "url": "/confilogi_pr/static/media/myriadpro-regular.bf59179a.woff2"
+  },
+  {
+    "revision": "f6cfada406910530f648d7a9b95fc295",
+    "url": "/confilogi_pr/static/media/myriadpro-regular.f6cfada4.woff"
+  },
+  {
+    "revision": "8f73ea08f55028cc3cf3402e52f0acbf",
+    "url": "/confilogi_pr/static/media/quatation-mark.8f73ea08.svg"
+  },
+  {
+    "revision": "3297fa2bfba895c7902456b5d4945e94",
+    "url": "/confilogi_pr/static/media/roboto-bold-webfont.3297fa2b.woff2"
+  },
+  {
+    "revision": "6a217a1a7956276cf848b87914183f2f",
+    "url": "/confilogi_pr/static/media/roboto-bold-webfont.6a217a1a.woff"
+  },
+  {
+    "revision": "552a71698a622eec569387b1eb157f69",
+    "url": "/confilogi_pr/static/media/roboto-light-webfont.552a7169.woff2"
+  },
+  {
+    "revision": "6d8326651b053d6e633f3f0bb5536361",
+    "url": "/confilogi_pr/static/media/roboto-light-webfont.6d832665.woff"
+  },
+  {
+    "revision": "1fc168f81b72130d02c4d94a55f958fc",
+    "url": "/confilogi_pr/static/media/roboto-regular-webfont.1fc168f8.woff2"
+  },
+  {
+    "revision": "99935f2c2ca3e9a74aae6b6f78b7950c",
+    "url": "/confilogi_pr/static/media/roboto-regular-webfont.99935f2c.woff"
+  },
+  {
+    "revision": "09d7e9e0c16699ed417da22c18bbbffe",
+    "url": "/confilogi_pr/static/media/three-dots.09d7e9e0.svg"
+  }
+]);
