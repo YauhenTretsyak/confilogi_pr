d33ae0fa367d954c9165e8e4abe566b9d39fc41f
Agata Nakonieczna <agnakonieczna@gmail.com>
2020-10-23 09:18:15 +0200

    mobile menu added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -31,7 +31,12 @@ const Header = () => {
                 alt='instagram-logo'
               />
             </div>
-            <img className='header_menu' src={threeDots} alt='menu' onClick={() => setShowMenu(showMenu === true ? false : true)}/> 
+            <img
+              className='header_menu'
+              src={threeDots}
+              alt='menu'
+              onClick={() => setShowMenu(showMenu === true ? false : true)}
+            />
           </div>
           <div className='logo'>
             <h1 className='logo_item'>Shine like a</h1>
@@ -39,19 +44,34 @@ const Header = () => {
           </div>
           <p className='header_text'>Poznaj pikantne fakty z życia gwiazd!</p>
           <span className='header_highlight'></span>
-          <nav className={showMenu ? 'header_navigation show' : 'header_navigation'}>
-          {
-            data.navigationLinks.map((link, index) => {
-              return <Navigation key={index} text={link} styleName={'header_navigation_item'}/>
-            } )
-          }
-          </nav>
-          <span className='header_highlight'></span>
+            <nav
+              className={
+                showMenu
+                  ? 'header_navigation header_navigation-show'
+                  : 'header_navigation'
+              }
+            >
+              {data.navigationLinks.map((link, index) => {
+                return (
+                  <Navigation
+                    key={index}
+                    text={link}
+                    styleName={'header_navigation_item'}
+                  />
+                );
+              })}
+            </nav>
+            <span
+              className={
+                showMenu
+                  ? 'header_highlight header_highlight-show'
+                  : 'header_highlight header_highlight-last'
+              }
+            ></span>
         </div>
       </header>
     </>
   );
-}
+};
 
 export default Header;
-
